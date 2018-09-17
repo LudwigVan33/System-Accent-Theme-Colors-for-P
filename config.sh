47362cc0302df96422306c7b9ddb698daabf6dd3
Ludwig33 <anthony.peche.m@gmail.com>
2018-09-16 20:39:10 -0500

    Correct module name on config.sh

--- config.sh
+++ config.sh
@@ -40,9 +40,9 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "     Moto Stock Sounds    "
-  ui_print "*******************************"
+  ui_print "********************************"
+  ui_print "System Accent Theme Colors for P"
+  ui_print "********************************"
 }
 
 ##########################################################################################
